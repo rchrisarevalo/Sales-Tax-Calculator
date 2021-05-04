9150ebdcafba3e4d72d7a286b854220f4a343a84
Ruben Christopher Arevalo <42070061+rchrisarevalo@users.noreply.github.com>
2021-05-03 20:02:01 -0500

    v1.1
    This is a new release of the Sales Tax Program. New updates were added to this program, with the most noticeable being the addition of new dialog boxes. This was done to make the program look more stylistic.

---
+++ Sales Taxes (all 50 states)/src/Tax.java
@@ -0,0 +1,720 @@
+// NOTE: These figures are based from information that I used to create this program on:
+// https://taxfoundation.org/2021-sales-taxes/
+
+
+public class Tax {
+	double calculateAlabama(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .04;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateAlaska(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .00;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateArizona(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .0560;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateArkansas(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .0650;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateCalifornia(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .0725;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateColorado(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .0290;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateConnecticut(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .0635;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateDelaware(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .00;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateDC(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .06;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateFlorida(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .06;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateGeorgia(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .04;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateHawaii(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .04;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateIdaho(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .06;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateIllinois(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .0625;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateIndiana(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .07;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateIowa(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .06;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateKansas(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .0650;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateKentucky(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .06;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateLouisiana(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .0445;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateMaine(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .0550;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateMaryland(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .06;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateMassachusetts(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .0625;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateMichigan(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .06;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateMinnesota(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .06875;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateMississippi(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .07;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateMissouri(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .04225;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateMontana(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .00;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateNebraska(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .0550;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateNevada(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .0685;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateNewHampshire(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .00;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateNewJersey(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .06625;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateNewMexico(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .05125;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateNewYork(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .04;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateNorthCarolina(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .0475;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateNorthDakota(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .05;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateOhio(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .0575;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateOklahoma(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .0450;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateOregon(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .00;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculatePennsylvania(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .06;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateRhodeIsland(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .07;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateSouthCarolina(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .06;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateSouthDakota(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .0450;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateTennessee(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .07;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateTexas(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .0625;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateUtah(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .0610;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateVermont(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .06;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateVirginia(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .0530;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateWashington(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .0650;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateWestVirginia(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .06;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateWisconsin(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .05;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+	double calculateWyoming(String p) {
+		
+		System.out.println("Calculating...");
+		
+		double price = Double.parseDouble(p);
+		
+		double tax = .05125;
+		double tax_calculation = tax * price;
+		double final_tax = price + tax_calculation;
+		
+		System.out.println("Calculation successful!");
+		
+		return final_tax;
+	}
+}
